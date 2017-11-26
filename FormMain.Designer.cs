7058dfe66f8315a87c1c7217ed85ea0d244b78de
Nicolas TURPIN <nturpin@hotmail.fr>
2017-11-26 11:43:03 +0100

    BASE

--- FormMain.Designer.cs
+++ FormMain.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace RSerie
 {
-    partial class Form1
+    partial class FormMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,9 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.SuspendLayout();
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormMain";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- FormMain.cs
+++ FormMain.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace RSerie
 {
-    public partial class Form1 : Form
+    public partial class FormMain : Form
     {
-        public Form1()
+        public FormMain()
         {
             InitializeComponent();
         }
