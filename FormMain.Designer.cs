308f1fee5bb374d3a0e85ffa79b0c68bf87841fc
Nicolas TURPIN <nturpin@hotmail.fr>
2017-11-26 12:49:04 +0100

    Archive

--- FormMain.Designer.cs
+++ FormMain.Designer.cs
@@ -29,21 +29,337 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.lbVolts_Label = new System.Windows.Forms.Label();
+            this.lbR1_Label = new System.Windows.Forms.Label();
+            this.lbR2_Label = new System.Windows.Forms.Label();
+            this.edVolts = new System.Windows.Forms.TextBox();
+            this.edR1 = new System.Windows.Forms.TextBox();
+            this.edR2 = new System.Windows.Forms.TextBox();
+            this.lbCOurantçLabel = new System.Windows.Forms.Label();
+            this.lbV1_Label = new System.Windows.Forms.Label();
+            this.lbV2_Label = new System.Windows.Forms.Label();
+            this.lbV2 = new System.Windows.Forms.Label();
+            this.lbV1 = new System.Windows.Forms.Label();
+            this.lbCourant = new System.Windows.Forms.Label();
+            this.btCalcul = new System.Windows.Forms.Button();
+            this.lbInfoCourant = new System.Windows.Forms.Label();
+            this.lbInfoV1 = new System.Windows.Forms.Label();
+            this.lbInfoV2 = new System.Windows.Forms.Label();
+            this.lbV1V2 = new System.Windows.Forms.Label();
+            this.lbV1V2_Label = new System.Windows.Forms.Label();
+            this.lbInfoV1V2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // lbVolts_Label
+            // 
+            this.lbVolts_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbVolts_Label.ForeColor = System.Drawing.Color.SteelBlue;
+            this.lbVolts_Label.Location = new System.Drawing.Point(12, 193);
+            this.lbVolts_Label.Name = "lbVolts_Label";
+            this.lbVolts_Label.Size = new System.Drawing.Size(130, 22);
+            this.lbVolts_Label.TabIndex = 0;
+            this.lbVolts_Label.Text = "Volts";
+            this.lbVolts_Label.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lbR1_Label
+            // 
+            this.lbR1_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbR1_Label.ForeColor = System.Drawing.Color.SteelBlue;
+            this.lbR1_Label.Location = new System.Drawing.Point(12, 216);
+            this.lbR1_Label.Name = "lbR1_Label";
+            this.lbR1_Label.Size = new System.Drawing.Size(130, 22);
+            this.lbR1_Label.TabIndex = 2;
+            this.lbR1_Label.Text = "R1 (Ohms)";
+            this.lbR1_Label.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lbR2_Label
+            // 
+            this.lbR2_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbR2_Label.ForeColor = System.Drawing.Color.SteelBlue;
+            this.lbR2_Label.Location = new System.Drawing.Point(12, 239);
+            this.lbR2_Label.Name = "lbR2_Label";
+            this.lbR2_Label.Size = new System.Drawing.Size(130, 22);
+            this.lbR2_Label.TabIndex = 4;
+            this.lbR2_Label.Text = "R2 (Ohms)";
+            this.lbR2_Label.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // edVolts
+            // 
+            this.edVolts.BackColor = System.Drawing.Color.White;
+            this.edVolts.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.edVolts.ForeColor = System.Drawing.Color.SteelBlue;
+            this.edVolts.Location = new System.Drawing.Point(153, 193);
+            this.edVolts.Name = "edVolts";
+            this.edVolts.Size = new System.Drawing.Size(99, 22);
+            this.edVolts.TabIndex = 1;
+            // 
+            // edR1
+            // 
+            this.edR1.BackColor = System.Drawing.Color.White;
+            this.edR1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.edR1.ForeColor = System.Drawing.Color.SteelBlue;
+            this.edR1.Location = new System.Drawing.Point(153, 216);
+            this.edR1.Name = "edR1";
+            this.edR1.Size = new System.Drawing.Size(99, 22);
+            this.edR1.TabIndex = 3;
+            // 
+            // edR2
+            // 
+            this.edR2.BackColor = System.Drawing.Color.White;
+            this.edR2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.edR2.ForeColor = System.Drawing.Color.SteelBlue;
+            this.edR2.Location = new System.Drawing.Point(153, 239);
+            this.edR2.Name = "edR2";
+            this.edR2.Size = new System.Drawing.Size(99, 22);
+            this.edR2.TabIndex = 5;
+            // 
+            // lbCOurantçLabel
+            // 
+            this.lbCOurantçLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCOurantçLabel.ForeColor = System.Drawing.Color.SteelBlue;
+            this.lbCOurantçLabel.Location = new System.Drawing.Point(13, 314);
+            this.lbCOurantçLabel.Name = "lbCOurantçLabel";
+            this.lbCOurantçLabel.Size = new System.Drawing.Size(130, 22);
+            this.lbCOurantçLabel.TabIndex = 7;
+            this.lbCOurantçLabel.Text = "Courant i (Amp)";
+            this.lbCOurantçLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lbV1_Label
+            // 
+            this.lbV1_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbV1_Label.ForeColor = System.Drawing.Color.SteelBlue;
+            this.lbV1_Label.Location = new System.Drawing.Point(13, 337);
+            this.lbV1_Label.Name = "lbV1_Label";
+            this.lbV1_Label.Size = new System.Drawing.Size(130, 22);
+            this.lbV1_Label.TabIndex = 10;
+            this.lbV1_Label.Text = "V1 (Volts)";
+            this.lbV1_Label.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lbV2_Label
+            // 
+            this.lbV2_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbV2_Label.ForeColor = System.Drawing.Color.SteelBlue;
+            this.lbV2_Label.Location = new System.Drawing.Point(13, 360);
+            this.lbV2_Label.Name = "lbV2_Label";
+            this.lbV2_Label.Size = new System.Drawing.Size(130, 22);
+            this.lbV2_Label.TabIndex = 13;
+            this.lbV2_Label.Text = "V2 (Volts)";
+            this.lbV2_Label.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lbV2
+            // 
+            this.lbV2.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.lbV2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbV2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbV2.ForeColor = System.Drawing.Color.SteelBlue;
+            this.lbV2.Location = new System.Drawing.Point(153, 360);
+            this.lbV2.Name = "lbV2";
+            this.lbV2.Size = new System.Drawing.Size(99, 22);
+            this.lbV2.TabIndex = 14;
+            this.lbV2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lbV1
+            // 
+            this.lbV1.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.lbV1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbV1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbV1.ForeColor = System.Drawing.Color.SteelBlue;
+            this.lbV1.Location = new System.Drawing.Point(153, 337);
+            this.lbV1.Name = "lbV1";
+            this.lbV1.Size = new System.Drawing.Size(99, 22);
+            this.lbV1.TabIndex = 11;
+            this.lbV1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lbCourant
+            // 
+            this.lbCourant.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.lbCourant.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbCourant.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbCourant.ForeColor = System.Drawing.Color.SteelBlue;
+            this.lbCourant.Location = new System.Drawing.Point(153, 314);
+            this.lbCourant.Name = "lbCourant";
+            this.lbCourant.Size = new System.Drawing.Size(99, 22);
+            this.lbCourant.TabIndex = 8;
+            this.lbCourant.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btCalcul
+            // 
+            this.btCalcul.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
+            this.btCalcul.ForeColor = System.Drawing.Color.SteelBlue;
+            this.btCalcul.Location = new System.Drawing.Point(153, 274);
+            this.btCalcul.Name = "btCalcul";
+            this.btCalcul.Size = new System.Drawing.Size(98, 32);
+            this.btCalcul.TabIndex = 6;
+            this.btCalcul.Text = "Calcul";
+            this.btCalcul.UseVisualStyleBackColor = true;
+            this.btCalcul.Click += new System.EventHandler(this.btCalcul_Click);
+            // 
+            // lbInfoCourant
+            // 
+            this.lbInfoCourant.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbInfoCourant.ForeColor = System.Drawing.Color.SteelBlue;
+            this.lbInfoCourant.Location = new System.Drawing.Point(258, 314);
+            this.lbInfoCourant.Name = "lbInfoCourant";
+            this.lbInfoCourant.Size = new System.Drawing.Size(425, 22);
+            this.lbInfoCourant.TabIndex = 9;
+            this.lbInfoCourant.Text = "i = U/R";
+            this.lbInfoCourant.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lbInfoV1
+            // 
+            this.lbInfoV1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbInfoV1.ForeColor = System.Drawing.Color.SteelBlue;
+            this.lbInfoV1.Location = new System.Drawing.Point(258, 337);
+            this.lbInfoV1.Name = "lbInfoV1";
+            this.lbInfoV1.Size = new System.Drawing.Size(425, 22);
+            this.lbInfoV1.TabIndex = 12;
+            this.lbInfoV1.Text = "V1 = R1 * i";
+            this.lbInfoV1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lbInfoV2
+            // 
+            this.lbInfoV2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbInfoV2.ForeColor = System.Drawing.Color.SteelBlue;
+            this.lbInfoV2.Location = new System.Drawing.Point(258, 359);
+            this.lbInfoV2.Name = "lbInfoV2";
+            this.lbInfoV2.Size = new System.Drawing.Size(425, 22);
+            this.lbInfoV2.TabIndex = 15;
+            this.lbInfoV2.Text = "V2 = R2 * i";
+            this.lbInfoV2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lbV1V2
+            // 
+            this.lbV1V2.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.lbV1V2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lbV1V2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbV1V2.ForeColor = System.Drawing.Color.SteelBlue;
+            this.lbV1V2.Location = new System.Drawing.Point(153, 383);
+            this.lbV1V2.Name = "lbV1V2";
+            this.lbV1V2.Size = new System.Drawing.Size(99, 22);
+            this.lbV1V2.TabIndex = 17;
+            this.lbV1V2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lbV1V2_Label
+            // 
+            this.lbV1V2_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbV1V2_Label.ForeColor = System.Drawing.Color.SteelBlue;
+            this.lbV1V2_Label.Location = new System.Drawing.Point(14, 383);
+            this.lbV1V2_Label.Name = "lbV1V2_Label";
+            this.lbV1V2_Label.Size = new System.Drawing.Size(130, 22);
+            this.lbV1V2_Label.TabIndex = 16;
+            this.lbV1V2_Label.Text = "V1 + V2 (Volts)";
+            this.lbV1V2_Label.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // lbInfoV1V2
+            // 
+            this.lbInfoV1V2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbInfoV1V2.ForeColor = System.Drawing.Color.SteelBlue;
+            this.lbInfoV1V2.Location = new System.Drawing.Point(258, 381);
+            this.lbInfoV1V2.Name = "lbInfoV1V2";
+            this.lbInfoV1V2.Size = new System.Drawing.Size(425, 22);
+            this.lbInfoV1V2.TabIndex = 18;
+            this.lbInfoV1V2.Text = "Delta V = V - (V1+V2)";
+            this.lbInfoV1V2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(288, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(188, 285);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 19;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label1.Location = new System.Drawing.Point(482, 125);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 55);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Vers entrée analogique ARDUINO";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.ForestGreen;
+            this.label2.Location = new System.Drawing.Point(482, 177);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 55);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "La valeur correspond à la tension aux bornes de R2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(695, 431);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lbInfoV1V2);
+            this.Controls.Add(this.lbV1V2);
+            this.Controls.Add(this.lbV1V2_Label);
+            this.Controls.Add(this.lbInfoV2);
+            this.Controls.Add(this.lbInfoV1);
+            this.Controls.Add(this.lbInfoCourant);
+            this.Controls.Add(this.btCalcul);
+            this.Controls.Add(this.lbV2);
+            this.Controls.Add(this.lbV1);
+            this.Controls.Add(this.lbCourant);
+            this.Controls.Add(this.lbV2_Label);
+            this.Controls.Add(this.lbV1_Label);
+            this.Controls.Add(this.lbCOurantçLabel);
+            this.Controls.Add(this.edR2);
+            this.Controls.Add(this.edR1);
+            this.Controls.Add(this.edVolts);
+            this.Controls.Add(this.lbR2_Label);
+            this.Controls.Add(this.lbR1_Label);
+            this.Controls.Add(this.lbVolts_Label);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormMain";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormMain_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lbVolts_Label;
+        private System.Windows.Forms.Label lbR1_Label;
+        private System.Windows.Forms.Label lbR2_Label;
+        private System.Windows.Forms.TextBox edVolts;
+        private System.Windows.Forms.TextBox edR1;
+        private System.Windows.Forms.TextBox edR2;
+        private System.Windows.Forms.Label lbCOurantçLabel;
+        private System.Windows.Forms.Label lbV1_Label;
+        private System.Windows.Forms.Label lbV2_Label;
+        private System.Windows.Forms.Label lbV2;
+        private System.Windows.Forms.Label lbV1;
+        private System.Windows.Forms.Label lbCourant;
+        private System.Windows.Forms.Button btCalcul;
+        private System.Windows.Forms.Label lbInfoCourant;
+        private System.Windows.Forms.Label lbInfoV1;
+        private System.Windows.Forms.Label lbInfoV2;
+        private System.Windows.Forms.Label lbV1V2;
+        private System.Windows.Forms.Label lbV1V2_Label;
+        private System.Windows.Forms.Label lbInfoV1V2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- FormMain.cs
+++ FormMain.cs
@@ -16,5 +16,58 @@ namespace RSerie
         {
             InitializeComponent();
         }
+
+        private void btCalcul_Click(object sender, EventArgs e)
+        {
+
+            float volts = float.Parse(edVolts.Text);
+            float R1 = float.Parse(edR1.Text);
+            float R2 = float.Parse(edR2.Text);
+            float resistanceGlobale = R1 + R2;
+
+            //Calcul du i
+            float courant = volts / resistanceGlobale;
+            lbCourant.Text = courant.ToString("0.0000000");
+
+            string infoCourant = "i = U/R";
+            infoCourant += " = " + volts.ToString("0.0") + " / (" + R1.ToString("0") + "+" + R2.ToString("0") + ")";
+            infoCourant += " = " + volts.ToString("0.0") + " / " + resistanceGlobale.ToString("0");
+            infoCourant += " = " + (volts/resistanceGlobale).ToString("0.0000000");
+            infoCourant += " Amp";
+            lbInfoCourant.Text = infoCourant;
+
+            //Calcul V1
+            float v1 = R1 * courant;
+            lbV1.Text = v1.ToString("0.00");
+
+            string infoV1 = "V1 = R1 * i";
+            infoV1 += " = " +  R1.ToString("0") + " * " + courant.ToString("0.0000000");
+            infoV1 += " Volts";
+            lbInfoV1.Text = infoV1;
+
+            
+            //Calcul V2
+            float v2 = R2 * courant;
+            lbV2.Text = v2.ToString("0.00");
+
+            string infoV2 = "V2 = R2 * i";
+            infoV2 += " = " + R2.ToString("0") + " * " + courant.ToString("0.0000000");
+            infoV2 += " Volts";
+            lbInfoV2.Text = infoV2;
+
+            //V1+V2
+            float V1V2 = v1 + v2;
+            lbV1V2.Text = V1V2.ToString("0.00");
+
+            string infoV1V2 = "Delta V = V - (V1+V2)";
+            infoV1V2 += " = " + (volts-V1V2).ToString("0.0000");
+            lbInfoV1V2.Text = infoV1V2;
+
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {
+            this.Text = "Resistances en sérise";
+        }
     }
 }
